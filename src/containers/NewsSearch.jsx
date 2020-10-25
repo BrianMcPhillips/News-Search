6fc0d9f83d8c9fd3ece4496a25c636e60974edda
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-24 22:39:23 -0700

    container finished articles listed to page

--- src/containers/NewsSearch.jsx
+++ src/containers/NewsSearch.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import Loading from '../components/Loading/Loading';
+import Search from '../components/Search/Search';
+import Articles from '../components/Articles/Articles';
+import { fetchArticles } from '../Services/fetchArticles';
 
 
 export default class NewsSearch extends Component {
+  state = {
+    input: '',
+    articles: [],
+    loading: false
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ input: target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+
+    fetchArticles(this.state.input)
+      .then(articles => {
+        this.setState({ articles });
+      })
+      .then(setTimeout(() => {
+        this.setState({ loading: false });
+      }, 3000));
+  }
+
   render() {
+    const { input, loading, articles } = this.state;
     return (
-      <Loading />
+      <div>
+        <Search
+          input={input}
+          onChange={this.handleChange}
+          onSubmit={this.handleSubmit} />
+        {loading ? <Loading /> : <Articles articles={articles} />}
+      </div>
     );
   }
 }
